5eb5ba0ec845209c8762fe1489489bf4fe753591
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-07-27 15:36:54 +0000

    Removed tank, tile and map.

--- tank/Barrel.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func _process(delta):
-	if Input.is_action_pressed("rotate_barrel_clockwise"):
-		print("clock")
-		rotation_degrees += 1
-	elif Input.is_action_pressed("rotate_barrel_counter_clockwise"):
-		print("anticlock")
-		rotation_degrees -= 1
