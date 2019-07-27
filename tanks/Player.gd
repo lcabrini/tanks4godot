a79beb76096033a4a2fb15e17fe36335a7d87af8
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-07-27 16:34:58 +0000

    Added input actions.

--- tanks/Player.gd
+++ tanks/Player.gd
@@ -3,13 +3,13 @@ extends "res://tanks/Tank.gd"
 func control(delta):
 	$Turret.look_at(get_global_mouse_position())
 	var rot_dir = 0
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("turn_right"):
 		rot_dir += 1
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("turn_left"):
 		rot_dir -= 1
 	rotation += rotation_speed * rot_dir * delta
 	velocity = Vector2()
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("forward"):
 		velocity = Vector2(speed, 0).rotated(rotation)
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("back"):
 		velocity = Vector2(-speed/2, 0).rotated(rotation)
