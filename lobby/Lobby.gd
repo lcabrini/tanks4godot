be3e6c3bcb1afd7036035c4b2bf4c1fb79634d2c
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-08-12 09:53:18 +0000

    Let clients connect from the lobby.

--- lobby/Lobby.gd
+++ lobby/Lobby.gd
@@ -4,6 +4,10 @@ func _on_Host_pressed():
 	$Join.disabled = true
 	network.create_server($Control/Nickname.text)
 	_load_map()
+	
+func _on_Join_pressed():
+	network.connect_to_server($Control/Nickname.text)
+	_load_map()
 
 func _on_Nickname_text_changed(new_text):
 	if $Control/Nickname.text == '':
@@ -15,3 +19,4 @@ func _on_Nickname_text_changed(new_text):
 		
 func _load_map():
 	get_tree().change_scene('res://maps/Map01.tscn')
+
