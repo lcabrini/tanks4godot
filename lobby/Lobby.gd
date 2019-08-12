e43150410b2de42777ea59c3bb5c233ae79d63f2
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-08-12 15:58:22 +0000

    Lobby work. Currently not working.

--- lobby/Lobby.gd
+++ lobby/Lobby.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
+var player_count = 0
+
 func _on_Host_pressed():
 	$Join.disabled = true
-	network.create_server($Control/Nickname.text)
-	_load_map()
+	Network.create_server($Control/Nickname.text)
+	_add_player()
 	
 func _on_Join_pressed():
-	network.connect_to_server($Control/Nickname.text)
-	_load_map()
+	Network.connect_to_server($Control/Nickname.text)
+	_add_player()
 
 func _on_Nickname_text_changed(new_text):
 	if $Control/Nickname.text == '':
@@ -17,6 +19,11 @@ func _on_Nickname_text_changed(new_text):
 		$Join.disabled = false
 		$Host.disabled = false
 		
-func _load_map():
-	get_tree().change_scene('res://maps/Map01.tscn')
+#func _load_map():
+#	get_tree().change_scene('res://maps/Map01.tscn')
 
+func _add_player():
+	player_count += 1
+	print("Player count: " + str(player_count))
+	if player_count > 1:
+		$Start.visible = true
