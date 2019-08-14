cb616d52ff403543ef4a1041c6660c799d287137
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-08-14 09:04:06 +0000

    Clean-up.

---
+++ gamestate.gd
@@ -0,0 +1,53 @@
+extends Node
+
+const DEFAULT_PORT = 14000
+const MAX_PEERS = 5
+
+signal server_created()
+signal player_list_changed()
+signal connection_succeeded()
+signal connection_failed()
+
+var nickname = ''
+var players = {}
+
+func _ready():
+	get_tree().connect('network_peer_connected', self, '_player_connected')
+	get_tree().connect('network_peer_disconnected', self, '_player_disconnected')
+	get_tree().connect('connected_to_server', self, '_connected_ok')
+	get_tree().connect('connection_failed', self, '_connected_fail')
+	get_tree().connect('server_disconnected', self, '_server_disconnected')
+
+func host_game(new_nickname):
+	nickname = new_nickname
+	var host = NetworkedMultiplayerENet.new()
+	host.create_server(DEFAULT_PORT, MAX_PEERS)
+	get_tree().set_network_peer(host)
+	emit_signal("server_created")
+	
+func join_game(ip, new_nickname):
+	nickname = new_nickname
+	var host = NetworkedMultiplayerENet.new()
+	host.create_client(ip, DEFAULT_PORT)
+	get_tree().set_network_peer(host) 
+	
+remote func register_player(nickname):
+	var id = get_tree().get_rpc_sender_id()
+	players[id] = nickname
+	emit_signal("player_list_changed")
+	
+func unregister_player(id):
+	players.erase(id)
+	
+func _player_connected(id):
+	print("player connected: " + str(id))
+	rpc_id(id, "register_player", nickname)
+	
+func _player_disconnected(id):
+	print("player disconnected: " + str(id))
+	
+func _connected_ok():
+	print("connection OK.")
+	
+func _connected_fail():
+	get_tree().set_network_peer(null)
--- lobby/Host.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-func _on_Host_pressed():
-	pass
-	
