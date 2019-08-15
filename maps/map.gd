7d3f6187af4e9aab9e79bba358186d00c0a20302
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-08-15 13:30:59 +0000

    Refactoring.

--- maps/map.gd
+++ maps/map.gd
@@ -5,11 +5,9 @@ func _ready():
 	new_player.name = str(get_tree().get_network_unique_id())
 	new_player.set_network_master(get_tree().get_network_unique_id())
 	get_tree().get_root().add_child(new_player)
-	var info = Network.player
 	new_player.init(info.nickname, info.position, false)
 	#set_camera_limits()
 	
-
 func set_camera_limits():
 	var map_limits = $Ground.get_used_rect()
 	var map_cellsize = $Ground.cell_size
