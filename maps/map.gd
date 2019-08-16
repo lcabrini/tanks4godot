ebe33335fdd98d6b16e07fb2f3a4fde0b8676d04
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-08-16 10:19:33 +0000

    Reworked most of player code.

--- maps/map.gd
+++ maps/map.gd
@@ -1,19 +1,19 @@
 extends Node2D
 
-func _ready():
-	var new_player = preload('res://tanks/player.tscn').instance()
-	new_player.name = str(get_tree().get_network_unique_id())
-	new_player.set_network_master(get_tree().get_network_unique_id())
-	get_tree().get_root().add_child(new_player)
+#func _ready():
+#	var new_player = preload('res://tanks/player.tscn').instance()
+#	new_player.name = str(get_tree().get_network_unique_id())
+#	new_player.set_network_master(get_tree().get_network_unique_id())
+#	get_tree().get_root().add_child(new_player)
 	#new_player.init(info.nickname, info.position, false)
 	#set_camera_limits()
 	
-func set_camera_limits():
-	var map_limits = $Ground.get_used_rect()
-	var map_cellsize = $Ground.cell_size
-	var cam = $Player/Camera2D
-	cam.limit_left = map_limits.position.x * map_cellsize.x
-	cam.limit_right = map_limits.end.x * map_cellsize.x
-	cam.limit_top = map_limits.position.y * map_cellsize.y
-	cam.limit_bottom = map_limits.end.y * map_cellsize.y
+#func set_camera_limits():
+#	var map_limits = $Ground.get_used_rect()
+#	var map_cellsize = $Ground.cell_size
+#	var cam = get_node('player_camera')
+#	cam.limit_left = map_limits.position.x * map_cellsize.x
+#	cam.limit_right = map_limits.end.x * map_cellsize.x
+#	cam.limit_top = map_limits.position.y * map_cellsize.y
+#	cam.limit_bottom = map_limits.end.y * map_cellsize.y
 	
--- tanks/player.gd
+++ tanks/player.gd
@@ -1,62 +1,40 @@
-extends "res://tanks/tank.gd"
+extends KinematicBody2D
 
-enum MovementKey {FORWARD, BACKWARD, STOPPED}
-enum RotationKey {LEFT, RIGHT, NONE}
+const MOTION_SPEED = 90.0
+const ROTATION_SPEED = 5
 
-slave var slave_movement_key = MovementKey.STOPPED
-slave var slave_rotation_key = RotationKey.NONE
-slave var slave_position = Vector2()
-slave var slave_turret_direction = Vector2()
+var velocity = Vector2()
+
+puppet var puppet_pos = Vector2()
+puppet var puppet_velocity = Vector2()
+puppet var puppet_rotation = 0
 
 func _ready():
+	puppet_pos = position
+	
 	if is_network_master():
-		$PlayerCamera.make_current()
+		get_node('player_camera').make_current()
 
-func control(delta):
-	pass
-	
 func _physics_process(delta):
-	var movement_key = MovementKey.STOPPED
-	var rotation_key = RotationKey.NONE
-	var turret_direction = Vector2()
-	
-	if is_network_master():
-		turret_direction = get_global_mouse_position()
-		if Input.is_action_pressed('turn_right'):
-			rotation_key = RotationKey.RIGHT
-		elif Input.is_action_pressed('turn_left'):
-			rotation_key = RotationKey.LEFT
-		if Input.is_action_pressed('forward'):
-			movement_key = MovementKey.FORWARD
-		elif Input.is_action_pressed('back'):
-			movement_key = MovementKey.BACKWARD
-		
-		rset_unreliable('slave_position', position)
-		rset('slave_movement_key', movement_key)
-		rset('slave_rotation_key', rotation_key)
-		rset('slave_turret_direction', turret_direction)
-	else:
-		_move(slave_movement_key, slave_rotation_key, slave_turret_direction, delta)
-		position = slave_position
-	
-	if get_tree().is_network_server():
-		pass
-		#network.update_position(int(name), position)
-		
-func _move(movement_key, rotation_key, turret_direction, delta):
-	$Turret.look_at(turret_direction)
-	
 	var rot_dir = 0
-	if rotation_key == RotationKey.RIGHT:
-			rot_dir += 1
-	if rotation_key == RotationKey.LEFT:
-		rot_dir -= 1
-	rotation += rotation_speed * rot_dir * delta
-	velocity = Vector2()
-	if movement_key == MovementKey.FORWARD:
-		velocity = Vector2(speed, 0).rotated(rotation)
-	if movement_key == MovementKey.BACKWARD:
-		velocity = Vector2(-speed/2, 0).rotated(rotation)
 		
-func init(nickname, start_position, is_slave):
-	global_position = start_position
+	if is_network_master():
+		get_node('turret').look_at(get_global_mouse_position())
+		if Input.is_action_pressed("turn_left"):
+			print("Turn left")
+			rot_dir += 1
+		if Input.is_action_pressed('turn_left'):
+			rot_dir -= 1
+		rotation += ROTATION_SPEED * rot_dir * delta
+		velocity = Vector2()
+		if Input.is_action_pressed('forward'):
+			velocity = Vector2(MOTION_SPEED, 0).rotated(rotation)
+		if Input.is_action_pressed('backward'):
+			velocity = Vector2(-MOTION_SPEED/2, 0).rotated(rotation)
+		rset('puppet_velocity', velocity)
+		rset('puppet_rotation', rotation)
+		rset('puppet_position', position)
+	else:
+		position = puppet_pos
+		velocity = puppet_velocity
+		rotation = puppet_rotation
