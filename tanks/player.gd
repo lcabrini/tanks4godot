4b4ab523fa958a78d7451666ac39a2cc04bef836
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-08-18 18:51:33 +0000

    The tanks are now able to rotate. They still cannot move, however.

--- tanks/player.gd
+++ tanks/player.gd
@@ -5,12 +5,12 @@ const ROTATION_SPEED = 5
 
 var velocity = Vector2()
 
-#puppet var puppet_pos = Vector2()
-puppet var puppet_velocity = Vector2()
-puppet var puppet_rotation = 0
+puppet var puppet_pos = Vector2()
+#puppet var puppet_velocity = Vector2()
+#puppet var puppet_rotation = 0
 
 func _ready():
-	#puppet_pos = position
+	puppet_pos = position
 	
 	if is_network_master():
 		get_node('player_camera').make_current()
@@ -25,24 +25,27 @@ func _physics_process(delta):
 	var rot_dir = 0
 		
 	if is_network_master():
-		#print("is master")
+		#print("control is master")
 		get_node('turret').look_at(get_global_mouse_position())
-		if Input.is_action_pressed("turn_left"):
-			print("Turn left")
+		if Input.is_action_pressed("turn_right"):
+			print("Turn right")
 			rot_dir += 1
 		if Input.is_action_pressed('turn_left'):
+			print("turn left")
 			rot_dir -= 1
 		rotation += ROTATION_SPEED * rot_dir * delta
 		velocity = Vector2()
 		if Input.is_action_pressed('forward'):
+			print("forward")
 			velocity = Vector2(MOTION_SPEED, 0).rotated(rotation)
 		if Input.is_action_pressed('backward'):
+			print("backward")
 			velocity = Vector2(-MOTION_SPEED/2, 0).rotated(rotation)
-		rset('puppet_velocity', velocity)
-		rset('puppet_rotation', rotation)
-		#rset('puppet_position', position)
+		#rset('puppet_velocity', velocity)
+		#rset('puppet_rotation', rotation)
+		rset('puppet_position', position)
 	else:
-		#position = puppet_pos
-		velocity = puppet_velocity
-		rotation = puppet_rotation
+		position = puppet_pos
+		#velocity = puppet_velocity
+		#rotation = puppet_rotation
 		#print("_physics_process: network client")
