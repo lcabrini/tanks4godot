5d2c55f1d0bfa335739bdc12e94a33692d723275
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-08-22 21:55:07 +0000

    Early work on damage.

--- explosion/explosion.gd
+++ explosion/explosion.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 
 #var started
 
@@ -19,3 +19,12 @@ func _on_explosion_finished():
 	print("done exploding")
 	get_node("animation").playing = false
 	queue_free()
+
+func _on_explosion_body_entered(body):
+	var distance = body.position.distance_to(position)
+	print("damage vector: " + str(distance))
+
+
+func _on_explode_zone_body_entered(body):
+	var distance = body.position.distance_to(position)
+	print("the damage vector: " + str(distance))
