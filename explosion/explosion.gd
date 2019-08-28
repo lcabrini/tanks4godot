0288c3467aa2e0a0b1270dbf2a3d30f6da6b030b
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-08-28 18:41:12 +0000

    Clean-up.

--- explosion/explosion.gd
+++ explosion/explosion.gd
@@ -1,21 +1,11 @@
 extends Area2D
 
-#var started
-
 func start(_position):
 	position = _position
-	#get_node('animation').connect("finished", self, "on_explosion_done")
 	get_node('animation').playing = true
 	get_node('sound').play()
-	#started = true
-	
-#func _process(delta):
-#	var a = get_node('animation')
-#	if started and not a.playing:
-#		queue_free()
 
 func _on_explosion_finished():
-	#print("done exploding")
 	get_node("animation").playing = false
 	queue_free()
 
@@ -24,5 +14,3 @@ func _on_explosion_body_entered(body):
 	var damage = 200 - distance
 	if body.has_method('get_hit'):
 		body.get_hit(damage)
-	#print("damage vector: " + str(distance))
-
